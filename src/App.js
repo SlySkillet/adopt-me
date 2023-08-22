ac7faa804cb1550f342003f545d66f2171d80e5a
Simon Conrad <simondbconrad@gmail.com>
2023-08-22 13:09:08 -0400

    add props to Pet component, install prettier

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, props.name),
         React.createElement("h2", {}, props.animal),
@@ -12,9 +12,21 @@ const App = () => {
         {},
         [
             React.createElement("h1", {}, "Adopt Me"),
-            React.createElement(Pet),
-            React.createElement(Pet),
-            React.createElement(Pet),
+            React.createElement(Pet, {
+                animal: "Dog",
+                name: "Luna",
+                breed: "Havanese",
+            }),
+            React.createElement(Pet, {
+                animal: "Bird",
+                name: "Pepper",
+                breed: "Cockatiel",
+            }),
+            React.createElement(Pet, {
+                animal: "Gerbil",
+                name: "Gerb",
+                breed: "Gerbil",
+            }),
         ]
     )
 }
